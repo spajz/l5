523b3aa63c1412c63c84ed6b85aca570629333eb
Djordje Dimitrijev <rasplinjac@gmail.com>
2015-04-02 21:03:50 +0200

    Bower update

--- public/build/assets/admin/js/added-bc214dfc.js
+++ public/build/assets/admin/js/added-bc214dfc.js
@@ -11,7 +11,7 @@ $.ajaxSetup({
     }
 });
 
-//Bootstrap file button
+// Bootstrap file button
 $(document).on('change', '.btn-file :file', function () {
     var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
