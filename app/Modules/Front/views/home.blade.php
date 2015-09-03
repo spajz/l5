822ba4c2c7c3e1ade0d7eea6ea836beb32d5e19e
Djordje Dimitrijev <rasplinjac@gmail.com>
2015-09-03 17:15:18 +0200

    Update

--- app/Modules/Front/views/home.blade.php
+++ app/Modules/Front/views/home.blade.php
@@ -6,13 +6,27 @@
 
     <div class="container-full">
         <div id="hero">
-                <img src="{{ asset($assetsDirFront . '/images/hero/fcb-hero-slide-01.jpg') }}" >
-                <img src="{{ asset($assetsDirFront . '/images/hero/fcb-hero-slide-02.jpg') }}" >
-                <img src="{{ asset($assetsDirFront . '/images/hero/fcb-hero-slide-03.jpg') }}" >
-                <img src="{{ asset($assetsDirFront . '/images/hero/fcb-hero-slide-04.jpg') }}" >
+            <div class="slider-box">
+                <img src="{{ asset($assetsDirFront . '/images/hero/hero-img-1500.png') }}" class="hero-bg-01">
+                <img src="{{ asset($assetsDirFront . '/images/hero/creativity.png') }}" class="hero-creativity">
+                <img src="{{ asset($assetsDirFront . '/images/hero/pobednik.png') }}" class="hero-pobednik">
             </div>
+            <div class="slider-box">
+                <img src="{{ asset($assetsDirFront . '/images/hero/hero-img-1500.png') }}" class="hero-bg-02">
+                <img src="{{ asset($assetsDirFront . '/images/hero/baloni.png') }}" class="hero-baloni">
+                {{--<img src="{{ asset($assetsDirFront . '/images/hero/konj.png') }}" class="hero-konj">--}}
+            </div>
+            <div class="slider-box">
+                <img src="{{ asset($assetsDirFront . '/images/hero/hero-img-1500.png') }}" class="hero-bg-03">
+                <img src="{{ asset($assetsDirFront . '/images/hero/change.png') }}" class="hero-change">
+                {{--<img src="{{ asset($assetsDirFront . '/images/hero/hram.png') }}" class="hero-hram">--}}
+            </div>
+            <div class="slider-box">
+                <img src="{{ asset($assetsDirFront . '/images/hero/hero-img-1500.png') }}" class="hero-bg-04">
+                <img src="{{ asset($assetsDirFront . '/images/hero/ljuljaska.png') }}" class="hero-ljuljaska">
+            </div>
+        </div>
     </div>
-
 
     <div class="container-fluid">
         <div class="row clearfix">
@@ -31,7 +45,6 @@
             </div>
         </div>
     </div>
-
 
     <div class="container-fluid bor-tb1">
 
@@ -79,6 +92,59 @@
 
 @stop
 
+@section('scripts_top')
+    @parent
+    <style type="text/css">
+        .slider-box {
+            position: relative;
+            /*height: 700px;*/
+        }
+
+        .slider-box img {
+            position: absolute;
+            width: 100%;
+            height: auto;
+        }
+
+        .hero-creativity{
+            opacity: 0;
+        }
+
+        .hero-bg-01{
+            background: url({{ asset($assetsDirFront . '/images/hero/hero-bg.jpg') }}) no-repeat 0 0;
+            -webkit-background-size: auto 100%;
+            -moz-background-size: auto 100%;
+            -o-background-size: auto 100%;
+            background-size: auto 100%;
+        }
+
+        .hero-bg-02{
+            background: url({{ asset($assetsDirFront . '/images/hero/hero-bg.jpg') }}) no-repeat 33.33333% 0;
+            -webkit-background-size: auto 100%;
+            -moz-background-size: auto 100%;
+            -o-background-size: auto 100%;
+            background-size: auto 100%;
+        }
+
+        .hero-bg-03{
+            background: url({{ asset($assetsDirFront . '/images/hero/hero-bg.jpg') }}) no-repeat 50% 0;
+            -webkit-background-size: auto 100%;
+            -moz-background-size: auto 100%;
+            -o-background-size: auto 100%;
+            background-size: auto 100%;
+        }
+
+        .hero-bg-04{
+            background: url({{ asset($assetsDirFront . '/images/hero/hero-bg.jpg') }}) no-repeat 100% 0;
+            -webkit-background-size: auto 100%;
+            -moz-background-size: auto 100%;
+            -o-background-size: auto 100%;
+            background-size: auto 100%;
+        }
+
+    </style>
+@stop
+
 @section('scripts_bottom')
     @parent
     <script type="text/javascript">
@@ -105,8 +171,59 @@
                     animateBg2(a);
                 });
             }
+        })
+
+        $(window).on('load', function(){
 
 
+            var h = $('.slider-box').find('img').first().height();
+
+            $('.slider-box').height(h);
+
+            $('#hero').slick({
+                autoplay: false,
+                autoplaySpeed: 4000,
+                fade: false,
+                arrows: false,
+                speed: 1000,
+                waitForAnimate: true,
+                pauseOnHover: false,
+            });
+
+
+            $('#hero img').on('click', function (e) {
+                $(this).closest('#hero').slick('slickNext')
+            });
+
+
+
+            $('.hero-pobednik').velocity({
+                left: "-=5%",
+            }, {
+//                loop: 4,
+                /* Wait 100ms before alternating back. */
+//                delay: 300,
+                duration: 4000,
+                easing: 'linear',
+            });
+
+            $('.hero-creativity').velocity({
+                right: "-=5%",
+                opacity: 1,
+//                width: "+=5rem", // Add 5rem to the current rem value
+//                height: "*=2" // Double the current height
+            }, {
+                easing: 'linear',
+                duration: 2000,
+            }).velocity({
+                        right: "-=5%",
+                        opacity: 0,
+//                width: "+=5rem", // Add 5rem to the current rem value
+//                height: "*=2" // Double the current height
+                    }, {
+                        easing: 'linear',
+                        duration: 2000,
+                    });
         })
     </script>
 @stop
